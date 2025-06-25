e6196cc2f144bd0c096391ad550a34466e05b44f
PedroSantana23 <ph.silva.santana@gmail.com>
2025-06-25 15:16:24 -0300

    JavaScript Básico -> (1) Primeiros Passos

---
+++ 03-javascript/scripts.js
@@ -0,0 +1,9 @@
+// Sintaxe correta.
+console.log("Olá, Pedro!")
+
+// Sintaxe incorreta.
+// con.sole.log("Olá, Pedro!")
+
+// No JavaScript Ponto e vírgula é opcional.
+console.log("Olá, Pedro!");
+console.log("Olá, Pedro!")
