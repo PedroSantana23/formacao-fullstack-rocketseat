804b44513cf34b86fd90744c4cceb9155669de1f
PedroSantana23 <ph.silva.santana@gmail.com>
2025-06-26 17:12:50 -0300

    JavaScript Básico -> (2) Variáveis e Constantes

--- 03-javascript/scripts.js
+++ 03-javascript/scripts.js
@@ -1,9 +1,28 @@
-// Sintaxe correta.
-console.log("Olá, Pedro!")
+/*
+    - Conversão de tipos (type casting ou type conversion):
+    ocorre quando você explicitamente transforma um valor de um
+    tipo para o outro. Isso é feito de forma consciente, usando 
+    funções ou métodos específicos para realizar a conversão.
+*/
 
-// Sintaxe incorreta.
-// con.sole.log("Olá, Pedro!")
+let value = "9"
+console.log(typeof Number(value)) // number
+console.log(typeof value) // string
 
-// No JavaScript Ponto e vírgula é opcional.
-console.log("Olá, Pedro!");
-console.log("Olá, Pedro!")
+let age = 18
+console.log(typeof age.toString()) // string
+console.log(typeof String(age)) // string
+console.log(typeof toString(age)) // string
+
+let option = 1
+console.log(Boolean(option)) // true
+console.log(typeof Boolean(option)) // boolean
+
+/*
+    - Coerção de tipos:
+    acontece de forma automática (implicitamente). O JavaScript
+    tenta automaticamente converter um dos valores para um tipo 
+    compatível antes de realizar a operação. 
+*/
+
+console.log(typeof ("10" + 5))
